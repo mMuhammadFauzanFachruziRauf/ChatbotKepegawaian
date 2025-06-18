d81cb49e1bdf1929b9a7e9597a7f0fd0bdf052ce
StarLol <155893972+mMuhammadFauzanFachruziRauf@users.noreply.github.com>
2025-06-19 01:10:34 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,6 +6,6 @@
 // dan kunci API agar tidak tercampur dengan logika kode lain.
 
 // API Key Anda dari Google AI Studio (Gemini)
-define('GEMINI_API_KEY', 'AIzaSyD-w1wIrDjXTQ8vw9Jjc5pMVwiTrki60_8');
+define('GEMINI_API_KEY', 'TOKEN');
 
 ?>
